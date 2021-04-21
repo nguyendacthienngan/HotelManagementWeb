29e473bbacbb69173ec0265752cdb61a649ab60c
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-04-21 16:57:26 +0700

    Fixing relation 1-to-1: Reservation has_one Payment

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :room
   belongs_to :client
   belongs_to :employee
-  has_one :reservation
+  has_one :payment
   has_many :reservation_details
   has_many :services, through: :reservation_details
 end

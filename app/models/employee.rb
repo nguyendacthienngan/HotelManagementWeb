3c92290d40e79a30f42fe3d1e495ba5294bdf88d
Bao Tien <ungbaotien0512@gmail.com>
2021-06-04 10:38:47 +0700

    Update employee & roomtype

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
-  has_many :reservations
+  has_many :reservations;
+  validates :name, length: { minimum: 2 }, uniqueness: true
+  validates :email, format: { with: /.*@.*/ }
 end

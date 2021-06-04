7c1ffc3272d5f8cf8640faee78e4af33b6d5ca58
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-04 16:40:45 +0700

    Passing room id to quick reserve room

--- app/controllers/room_diagram_controller.rb
+++ app/controllers/room_diagram_controller.rb
@@ -4,4 +4,8 @@ class RoomDiagramController < ApplicationController
     @rooms = Room.all
   end
 
+  def quick_reserve_room
+    @room_type_id = Room.find(params[:room_id]).room_type_id
+    @room_type_name = RoomType.find(@room_type_id).name;
+  end
 end

852a8c3cff445eb22202a3a1e7ef11716cfbf060
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-12 01:55:03 +0700

    Search with dropdown options

--- app/controllers/room_diagram_controller.rb
+++ app/controllers/room_diagram_controller.rb
@@ -2,16 +2,42 @@ include ActionView::Helpers::NumberHelper
 
 class RoomDiagramController < ApplicationController
   def index
+    @search_types = {
+      "Tên phòng" => "0",
+      "Tầng" => "1",
+      "Loại phòng" => "2"
+    }
     @floors = [1,2,3]
+    puts "Chosen Type"
+    puts params[:chosen_type]
+    puts "Name"
+    puts params[:name]
 
-    @rooms = if params[:name]
-               Room.where('name LIKE ?', "%#{params[:name]}%")
-             else
-               Room.all
-             end
-    puts @rooms.inspect
-    # puts "INDEX ROOM "
-    # puts @rooms.inspect
+    search_input = params[:name]
+    chosen_type = params[:chosen_type]
+    @rooms =
+      if search_input
+        if chosen_type
+          if chosen_type == "0"
+            Room.where('name LIKE ?', "%#{search_input}%")
+          else
+            if chosen_type == "1"
+              Room.where(floor: search_input)
+            else
+              room_t_id = find_room_type_id(search_input)
+              if (room_t_id != -1)
+                Room.where(room_type_id: room_t_id)
+              else
+                Room.all
+              end
+            end
+          end
+        else
+          Room.all
+        end
+      else
+        Room.all
+      end
   end
 
   def helper
@@ -19,15 +45,14 @@ class RoomDiagramController < ApplicationController
       include ActionView::Helpers::NumberHelper
     end.new
   end
-  def search
-    puts "SEARCH NAME: "
-    puts params[:name]
-    @rooms = if params[:name]
-               Room.where('name LIKE ?', "%#{params[:name]}%")
-             else
-               Room.all
-             end
-    puts @rooms.inspect
+
+  def find_room_type_id(room_type_name)
+    room_type = RoomType.where(name: room_type_name)
+    if (room_type)
+      return room_type.pluck(:id)
+    else
+      return -1
+    end
   end
 
   def quick_reserve_room
@@ -52,6 +77,6 @@ class RoomDiagramController < ApplicationController
 
 
   def room_params
-    params.require(:room).permit(:name)
+    params.require(:room).permit(:name, :chosen_type)
   end
 end

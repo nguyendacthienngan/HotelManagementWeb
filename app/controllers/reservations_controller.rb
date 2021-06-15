9e7f105e3bea6cdf2b72a4129f55a7947acafaa9
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-15 18:29:21 +0700

    Passing values in previous step

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -78,11 +78,25 @@ class ReservationsController < ApplicationController
       @reservation.next_step
     end
     session[:reservation_step] = @reservation.current_step
+
+    @room_id = session[:reservation_params]["room_id"]
+    @room_name = Room.find(@room_id).name
+    @room_type_id = Room.find(@room_id).room_type_id
+    @room_type_name = RoomType.find(@room_type_id).name
     @payment_type = @@payment_type
     @payment_type_view = convert_nested_hash_to_text(@payment_type)
     @reservation_type = @@reservation_types
     @reservation_type_view = convert_nested_hash_to_text(@reservation_type)
 
+    @payment_type = @@payment_type
+    @payment_type_view = convert_nested_hash_to_text(@payment_type)
+    @reservation_type = @@reservation_types
+    @reservation_type_view = convert_nested_hash_to_text(@reservation_type)
+
+    @room_price = RoomPrice.where(room_type_id: @room_type_id, price_type: 2)
+    @room_price = @room_price.pluck(:price).to_s
+    @room_price = @room_price.tr('[]', '')
+    @room_price = number_to_currency(@room_price, unit: "VND",  format: "%n %u")
     render "new"
   end
 

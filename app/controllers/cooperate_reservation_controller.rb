21f6bbc9744c5fc390a3409df0254a602e9637c7
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-27 16:30:41 +0700

    Editing cooperation reservation view

--- app/controllers/cooperate_reservation_controller.rb
+++ app/controllers/cooperate_reservation_controller.rb
@@ -4,8 +4,16 @@ class CooperateReservationController < ApplicationController
   end
   def new
     @reservation = Reservation.new
+
+    @rooms
+
     @search_result = {}
+
+
+    @room_types = RoomType.all
     @gender = @@gender
     @gender = convert_nested_hash_to_text(@gender)
+
+
   end
 end

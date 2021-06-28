30be2e139f35dbbe2aceed23d7d67575cd96a112
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-06-28 20:32:08 +0700

    Passing params in Create method

--- app/controllers/cooperate_reservation_controller.rb
+++ app/controllers/cooperate_reservation_controller.rb
@@ -5,12 +5,40 @@ class CooperateReservationController < ApplicationController
   end
   def new
     @reservation = Reservation.new
+    @payment_type = @@payment_type
+    @payment_type_view = convert_nested_hash_to_text(@payment_type)
     filter_room_type = params[:room_type_id]
+
     if filter_room_type
       @room_types = RoomType.where(id: filter_room_type)
     else
       @room_types = RoomType.all
     end
+
+    arrival_date = params[:arrival_date]
+    leave_date = params[:leave_date]
+
+    if arrival_date
+      @arrival_date = Time.parse(arrival_date).getutc
+    else
+      @arrival_date = DateTime.now
+    end
+
+    if leave_date
+      @leave_date = Time.parse(leave_date).getutc
+    else
+      @leave_date = DateTime.now
+    end
+
+    #Xem lại trạng thái của đặt: Là đang đặt thôi chứ chưa nhận phòng?
+    @unreserved_rooms = Reservation.where(["status = 1 and (arrival_date < ? and leave_date > ?)", "#{@arrival_date}", "#{@leave_date}"])
+    @unreserved_rooms = @unreserved_rooms.pluck(:room_id)
+
+    @rooms_status_2 = []
+    @unreserved_rooms.each do |r|
+      @rooms_status_2.push(Room.find(r))
+    end
+
     @room_prices = {}
     @room_types.each do |r_t|
       room_price = RoomPrice.where(room_type_id: r_t.id, price_type: 2).pluck(:price)
@@ -18,20 +46,47 @@ class CooperateReservationController < ApplicationController
         @room_prices["#{r_t.name}"] = currency_name(room_price.to_s)
       end
     end
-    @payment_type = @@payment_type
-    @payment_type_view = convert_nested_hash_to_text(@payment_type)
     @gender = @@gender
     @gender = convert_nested_hash_to_text(@gender)
   end
 
+
   def create
     @reservation = Reservation.new(reservation_params)
-    filter_room_type = params[:room_type_id]
+
+    @payment_type = @@payment_type
+    @payment_type_view = convert_nested_hash_to_text(@payment_type)
+    filter_room_type = reservation_params["room_type_id"]
     if filter_room_type
       @room_types = RoomType.where(id: filter_room_type)
     else
       @room_types = RoomType.all
     end
+
+    arrival_date = reservation_params["arrival_date"]
+    leave_date = reservation_params["leave_date"]
+
+    if arrival_date.nil?
+      @arrival_date = Time.parse(arrival_date).getutc
+    else
+      @arrival_date = DateTime.now
+    end
+
+    if leave_date.nil?
+      @leave_date = Time.parse(leave_date).getutc
+    else
+      @leave_date = DateTime.now
+    end
+
+    #Xem lại trạng thái của đặt: Là đang đặt thôi chứ chưa nhận phòng?
+    @unreserved_rooms = Reservation.where(["status = 1 and (arrival_date < ? and leave_date > ?)", "#{@arrival_date}", "#{@leave_date}"])
+    @unreserved_rooms = @unreserved_rooms.pluck(:room_id)
+
+    @rooms_status_2 = []
+    @unreserved_rooms.each do |r|
+      @rooms_status_2.push(Room.find(r))
+    end
+
     @room_prices = {}
     @room_types.each do |r_t|
       room_price = RoomPrice.where(room_type_id: r_t.id, price_type: 2).pluck(:price)
@@ -39,10 +94,9 @@ class CooperateReservationController < ApplicationController
         @room_prices["#{r_t.name}"] = currency_name(room_price.to_s)
       end
     end
-    @payment_type = @@payment_type
-    @payment_type_view = convert_nested_hash_to_text(@payment_type)
     @gender = @@gender
     @gender = convert_nested_hash_to_text(@gender)
+
 
     respond_to do |format|
       if @reservation.save
@@ -54,9 +108,16 @@ class CooperateReservationController < ApplicationController
       end
     end
   end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
   def reservation_params
     params.permit(:price_type_id, :arrival_date, :leave_date, :client_name, :client_citizen_id, :children, :adults, :employee_id, :room_id, :status, :reservation_type,:check_in_date,
                   payment_attributes:[:id, :temp_total, :reservation_date, :deposit, :is_paid, :payment_type],
                   client_attributes:[:id, :name, :citizen_id, :gender, :nationality, :date_of_birth, :email, :client_type, :phone_number ])
   end
 end
+

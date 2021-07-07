a3f9c34ebcd375b8fa3a1aced848272794004618
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-07-07 23:35:53 +0700

    Calculate total automatically

---
+++ app/javascript/packs/cooperate_reservation.js
@@ -0,0 +1,29 @@
+function convertCurrencyToPrice(currency)
+{
+    return currency.replace('VND','').replace(',','')
+}
+function calculateTotal()
+{
+    let total = 0
+    let prices = $('.room_price')
+    prices.each(function (i, obj){
+        let price = convertCurrencyToPrice($( this ).text())
+        total += parseInt(price)
+    })
+    let children_values = $('.children')
+    children_values.each(function (i, obj){
+            let children = $( this ).val()
+            let adults = $( this ).closest('.card-body').find('.adults').val()
+            let adultPrice = $( this ).closest('.card-body').find('.adults_price').text()
+            let childrenPrice = $( this ).closest('.card-body').find('.children_price').text()
+
+            total += children * parseInt(childrenPrice);
+            total += adults * parseInt(adultPrice);
+            $('#temp_total').val(total)
+        }
+    )
+}
+
+$(document).ready(function(){
+    calculateTotal()
+});
